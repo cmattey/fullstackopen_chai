1d789ffab0cba61bef902da1b3bd12838fb8832e
cmattey <cmattey@dons.usfca.edu>
2019-09-24 11:26:29 -0700

    Upto 1.10 unicafe step5,No stats feedback and stats in separate component

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,15 +10,35 @@ const Button = ({handleClick, text}) => {
   )
 }
 
-const Stat = ({text, value}) => {
-  if (isNaN(value)){
-    value = "Enter Feedback to calculate "+text
-  }
-  return (
-    <div>
-      <div>{text}: {value}</div>
-    </div>
+const Statistic = ({text,value}) => {
+  return(
+    <div>{text}: {value}</div>
   )
+}
+
+const Stat = ({stat_obj}) => {
+  let good = stat_obj["good"]
+  let neutral = stat_obj["neutral"]
+  let bad = stat_obj["bad"]
+
+  let total = good+neutral+bad
+  let average = (good-bad)/total
+  let positive_percent = (good/total)*100
+
+  if (good===0 && neutral===0 && bad===0){
+    return <div>No feedback given</div>
+  }
+  else{
+    return (
+      <div>
+        <Statistic text = "Good" value = {good}/>
+        <Statistic text = "Neutral" value = {neutral}/>
+        <Statistic text = "Bad" value = {bad}/>
+        <Statistic text = "Average" value = {average}/>
+        <Statistic text = "Positive Percent" value = {positive_percent}/>
+      </div>
+    )
+  }
 }
 
 
@@ -28,7 +48,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + bad + neutral
   const handleClick = (func, value) => () => {
     func(value)
   }
@@ -43,12 +62,7 @@ const App = () => {
         </div>
         <h3>Statistics</h3>
         <div>
-          <Stat text="Good" value={good}/>
-          <Stat text="Neutral" value={neutral}/>
-          <Stat text="Bad" value={bad}/>
-          <Stat text="All" value={total}/>
-          <Stat text="Average" value={(good+bad*(-1))/total}/>
-          <Stat text="Positive Percentage" value={good/total}/>
+          <Stat stat_obj={{"good":good,"neutral":neutral,"bad":bad}}/>
         </div>
     </div>
   )

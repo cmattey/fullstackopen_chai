257d2827ae399f999e9950ab5c96a31dcea00f5d
cmattey <cmattey@dons.usfca.edu>
2019-12-21 17:38:38 -0800

    Ex2.8 added phone number to the interface, with it's own event handler

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -3,44 +3,58 @@ import Note from './Note.js'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    'Arto Hellas'
+    {name:'Arto Hellas', number:1234}
   ])
   const [ newName, setNewName ] = useState('')
-
+  const [ newNumber, setNewNumber ] = useState('')
 
   const rows = () => persons.map(person =>
-    <Note key={person} person={person} />
+    <Note key={person.name} person={person} />
   )
 
-  const addName = (event) => {
+  const addInfo = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
 
-    if(persons.includes(newName)){
+    const names = persons.map(person => person.name)
+
+    if(names.includes(newName)){
       console.log("duplicate")
       window.alert(`${newName} is already added to phonebook`)
     }
     else{
-      setPersons(persons.concat(newName))
+      const personObj = {name:newName,
+                      number:newNumber}
+
+      setPersons(persons.concat(personObj))
     }
 
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumChange = (event) => {
+    // console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addInfo}>
         <div>
           name: <input
                 value={newName}
                 onChange={handleNameChange}/>
         </div>
+          number: <input
+                  value = {newNumber}
+                  onChange={handleNumChange}/>
         <div>
           <button type="submit">add</button>
         </div>
--- part2/phonebook/src/components/Note.js
+++ part2/phonebook/src/components/Note.js
@@ -4,7 +4,7 @@ const Note = ({person}) => {
 
   return (
     <div>
-      {person}
+      {person.name}: {person.number}
     </div>
   )
 }

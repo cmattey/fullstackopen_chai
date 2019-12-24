459a4abaf3863ce59cdea5fdb970dde7f161e576
cmattey <cmattey@dons.usfca.edu>
2019-12-23 21:27:15 -0800

    Ex2.12 Initial commit for country_info using restcountries.eu

---
+++ part2/country_info/src/components/App.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const App = () => {
+
+  return (
+    <div>
+      <h3>Search countries</h3>
+      <div>
+        Find countries: <input />
+      </div>
+
+      <div>
+      <h4>Results</h4>
+      </div>
+      
+
+    </div>
+  )
+
+}
+
+export default App;
